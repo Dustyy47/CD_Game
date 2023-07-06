f28bc1e0d1351072f87083ba9894d10a48e00e9a
Dmitry <109687670+Dustyy47@users.noreply.github.com>
2023-07-06 22:14:24 +0300

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      main: ["Product sans"],
+    },
+    colors: {
+      black: "#171717",
+      greyLight: "#2C2C2C",
+      bg: "#222222",
+      lime: "#CCFF00",
+      yellow: "#FFD600",
+    },
     extend: {},
   },
   plugins: [],
