9e757ee5d2f83b7831d547c4b36cad343f24d8db
Dmitry <109687670+Dustyy47@users.noreply.github.com>
2023-07-09 19:54:50 +0300

    styles and variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      lg: "1200px",
+      md: "720px",
+      sm: "320px",
+    },
     fontFamily: {
       main: ["Product sans"],
     },
     colors: {
+      white: "white",
       black: "#171717",
       greyLight: "#2C2C2C",
       grey: "#222222",
       lime: "#CCFF00",
       yellow: "#FFD600",
+      red: "#FF2424"
     },
     borderRadius: {
       common: "0.3125rem",
